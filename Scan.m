82b597e11712eb7f4122f379d8ae0abf3cb0b1c8
Cheng Zhang <cz2398@columbia.edu>
2015-10-29 14:49:19 -0400

    draft2

--- Scan.m
+++ Scan.m
@@ -1,6 +1,8 @@
 function  Scan(serPort)
     global Map;
     global cur_locat;
+    global total_x_dist;
+    global total_y_dist;
     init();
     while(1)
         % Whether the Map is searched completely
@@ -15,9 +17,11 @@ function  Scan(serPort)
             current = goal;
             goal = next(current);
         end
-        move(serPort,cur_locat,goal);
+        distance = align(serPort,[total_x_dist,total_y_dist],transf(goal,1));
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %   BUG 2(serPort,distance)
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         cur_locat = goal;
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
         Map(cur_locat(1),cur_locat(2)) = 1;
         Map_plot();
 %        pause(0.2);
@@ -29,14 +33,30 @@ function init()
     global start_locat;
     global Map_size;
     global current_angle;
+    global total_x_dist;
+    global total_y_dist;
+    global para;            % Scale
+    global status_unexplored;
+    global status_obstacle;
+    global status_vacant;
+    
+    para = 0.3;
+    status_unexplored = 0;
+    status_obstacle = 0.5;
+    status_vacant = 1;
+    
+    total_x_dist = 0;
+    total_y_dist = 0;
     current_angle = 0;
+    
     Map_size = [11,11];     % The last row/column is not used in colormap
     start_locat = [5,5];
     cur_locat = start_locat;
     Map = zeros(Map_size(1),Map_size(2));
-    Map(end,:)=1; Map(:,end)=1;
-    Map(start_locat(1),start_locat(2)) = 1;
-    Map(2:4,2:4)=0.5; Map(6:10,6:10)=0.5;
+    Map(end,:)=1; Map(:,end) = status_obstacle;
+    Map(start_locat(1),start_locat(2)) = status_vacant;
+    Map(2:4,2:4)=status_obstacle; 
+    Map(6:10,6:10)=status_obstacle;
     Map_plot();
 end
 
@@ -80,8 +100,9 @@ function status = valid(locat)
         status = 0;
     end
 end
-function move(serPort,current,goal)
+function align(serPort,current,goal)
     global current_angle;
+    global para;
     if goal(2)<=current(2)
         theta = acos(dot(goal-current,[1,0])/norm(goal-current))-current_angle;
         turnAngle (serPort, 0.4, 180*theta/pi);
@@ -93,7 +114,19 @@ function move(serPort,current,goal)
         display(180*theta/pi)
         current_angle = -acos(dot(goal-current,[1,0])/norm(goal-current));
     end
-    travelDist (serPort, 0.4, norm(goal-current)*0.3);      % The parameter may need to change.
+    travelDist (serPort, 0.4, norm(goal-current)*para);      % The parameter may need to change.
     display(current_angle);
     pause(0.2);
+end
+
+function loca2 = transf(loca1,flag)
+    global start_locat;
+    global para;
+    % From discrete to continuous
+    if flag==1
+        loca2 = (loca1-start_locat)*para;
+    % From continuous to discrete
+    else
+        loca2 = local/para + start_locat;
+    end   
 end
