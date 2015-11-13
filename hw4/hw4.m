6672f9a3cfdd0d2c4dec842327baeadffe3f9140
Lucy Lin <Lucy@cardinalblue.com>
2015-11-13 04:16:04 -0500

    fix intersect function, initial v graph algo

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -2,18 +2,32 @@ function hw4()
 
     global coordinates
     global dimension
+    global n_total_vertices
+    global start_point
+    global goal_point
+    global E_obstacles
+    global figHandle
+    global wall
     
-    
-    fileID = fopen('hw4_world_and_obstacles_convex.txt','r');
+    E_obstacles = [];
+    n_total_vertices = 0;
+    fileID = fopen('hw4_world_and_obstacles_convex_copy.txt','r');
     formatSpec = '%f';
     coor_list = fscanf(fileID,formatSpec);
-    N = coor_list(1);
+    N = coor_list(1)-1;
     idx = 2;
     dimension = zeros(1, N)
     coordinates = zeros(20, 2*N);
-    point_list_x = [];
-    point_list_y = [];
-    total_count = 0;
+    
+    % store wall vertices somewhere else
+    wall = [] 
+    m = coor_list(idx);
+    idx = idx + 1;
+    for i = 1:m
+        wall = [wall; [coor_list(idx), coor_list(idx+1)]];
+        idx = idx + 2;
+    end
+    
     for n = 1:N
         m = coor_list(idx);
         dimension(n) = m;
@@ -23,12 +37,24 @@ function hw4()
             
             idx = idx + 1;
             coordinates(i, 2 * n) = coor_list(idx);
+            
+            n_total_vertices = n_total_vertices + 1;
         end
         idx = idx + 1;
     end
     
     % plot
     figHandle = figure;
+    
+    % wall
+    x = wall(:, 1);
+    y = wall(:, 2);
+    plot(x,y,'g');
+    hold on;
+    plot([wall(end, 1), wall(1, 1)], [wall(end, 2), wall(1, 2)], 'g');
+    hold on;
+    
+    % obstacles
     for i = 1:N
         x = coordinates(1:dimension(i), 2*i-1);
         y = coordinates(1:dimension(i), 2*i);
@@ -37,17 +63,19 @@ function hw4()
         plot([x(end), x(1)], [y(end), y(1)], 'g');
         hold on;
     end
-    axis equal;
     
+    % start, goal
     fileID = fopen('hw4_start_goal.txt','r');
     formatSpec = '%f';
     start_end = fscanf(fileID,formatSpec);
     start_point = [start_end(1), start_end(2)];
-    end_point = [start_end(3), start_end(4)];
+    goal_point = [start_end(3), start_end(4)];
     plot(start_point(1), start_point(2), '*', 'MarkerEdgeColor','r', 'MarkerSize', 4);
     hold on;
-    plot(end_point(1), end_point(2), '*', 'MarkerEdgeColor','b', 'MarkerSize', 4);
+    plot(goal_point(1), goal_point(2), '*', 'MarkerEdgeColor','b', 'MarkerSize', 4);
     axis equal;
+    
+    build_vgraph();
     
 end
 
@@ -55,11 +83,110 @@ function build_vgraph()
     global coordinates
     global dimension
     global complete_vertex_list
+    global n_total_vertices
+    global E_map
+    global V_graph
+    global start_point
+    global goal_point
+    global E_obstacles
+    global figHandle
+    global wall
+     
+    N = size(dimension);
+    complete_vertex_list = [];
     
-    for i = 2:N
+    E_map = zeros(n_total_vertices + 2 + size(wall, 1), n_total_vertices + 2, size(wall, 1));
+    
+    idx = 1;
+    for i = 1:N(2)
         d = dimension(i);
+        complete_vertex_list = [complete_vertex_list; coordinates(1:d, 2*i-1:2*i)];
+            
         for j = 1:d
-            complete_vertex_list = [complete_vertex_list; [coordinates(1:dimension(i), 2*i-1:2*i)];
+            
+            if j > 1
+                E_map(idx, idx - 1) = 1;
+                E_map(idx - 1, idx) = 1;
+                E_obstacles = [E_obstacles; [idx, idx - 1]];
+            end
+            idx = idx + 1;
+        end
+        E_map(idx - 1, idx - d) = 1;
+        E_map(idx - d, idx - 1) = 1;
+        E_obstacles = [E_obstacles; [idx - 1, idx - d]];
     end
+    
+    % put in start and goal points
+    complete_vertex_list = [complete_vertex_list; start_point; goal_point];
+    idx = idx + 2;
+    
+    % put back wall vertices
+    d = size(wall, 1);
+    for i = 1:d
+        complete_vertex_list = [complete_vertex_list; wall(i, :)];
+        if i > 1
+            E_map(idx, idx - 1) = 1;
+            E_map(idx - 1, idx) = 1;
+            E_obstacles = [E_obstacles; [idx, idx - 1]];
+        end
+        idx = idx + 1;       
+    end
+    E_map(idx - 1, idx - d) = 1;
+    E_map(idx - d, idx - 1) = 1;
+    E_obstacles = [E_obstacles; [idx - 1, idx - d]];
+    
+    
+    
+%     figure;
+%     imagesc(E_map);
+    
+    V_graph = zeros(n_total_vertices + 2, n_total_vertices + 2);
+    
+    % if edge(i, j) doesn't intersect with obstacles, add edge(i, j) to
+    % v_graph
+    figure(figHandle);
+    t = 0;
+    for i = 1 : n_total_vertices + 2
+       for j = i + 1 : n_total_vertices + 2
+           if E_map(i, j) == 1
+               continue;
+           end
+           add = true;
+           for k = 1:size(E_obstacles, 1)
+               point1 = complete_vertex_list(E_obstacles(k, 1), :);
+               point2 = complete_vertex_list(E_obstacles(k, 2), :);
+               % has intersection
+               if isIntersect([point1; point2], ...
+                       [complete_vertex_list(i, :);...
+                        complete_vertex_list(j, :)]) == true
+                  add = false;
+                  break;
+               end              
+           end
+           if add
+               V_graph(i, j) = 1;
+               V_graph(j, i) = 1;
+               t = t + 1
+               plot([complete_vertex_list(i, 1), complete_vertex_list(j, 1)],...
+                   [complete_vertex_list(i, 2), complete_vertex_list(j, 2)],...
+                   'b');
+               hold on;
+           end
+           
+       end
+    end
+    
+    % plot V graph
+%     figure(figHandle);
+%     for i = 1 : n_total_vertices + 2
+%        for j = i + 1 : n_total_vertices + 2
+%            if V_graph(i, j) == 1
+%                plot([complete_vertex_list(i, 1), complete_vertex_list(j, 1)],...
+%                    [complete_vertex_list(i, 2), complete_vertex_list(j, 2)],...
+%                    'b');
+%            end
+%        end
+%     end
+    
     
 end
--- hw4/isIntersect.m
+++ hw4/isIntersect.m
@@ -1,33 +1,93 @@
 function isTrue = isIntersect(line1, line2)
+    % local functions
     isTrue = true;
     slope = @(line) (line(2,2) - line(1,2))/(line(2,1) - line(1,1));
-    m1 = slope(line1);
-    m2 = slope(line2);
     
-    % check parallel case ?
-    if m1 == m2
-        isTrue = false;
+    intersect = @(line,m) line(1,2) - m*line(1,1);
+    
+    isPointXInside = @(xint,myline) ...
+    (xint >= myline(1,1) && xint <= myline(2,1)) || ...
+    (xint >= myline(2,1) && xint <= myline(1,1));
+
+    isPointYInside = @(yint,myline) ...
+    (yint >= myline(1,2) && yint <= myline(2,2)) || ...
+    (yint >= myline(2,2) && yint <= myline(1,2));
+    
+    closeTo = @(point1, point2) (abs(point1-point2)<0.001);
+        
+    m1 = slope(line1);
+    m2 = slope(line2);  
+    
+    % vertical line segment
+    if line1(2, 1) == line1(1, 1) && line2(2, 1) ~= line2(1, 1)
+        b = line2(1,2)-m2*line2(1,1);
+        t = line1(1, 1)*m2 + b;
+        v = (line1(1, 2) - t) * (line1(2, 2) - t);
+        if  v < 0 && abs(line1(1, 2) - t) > 0.01 && abs(line1(2, 2) - t) > 0.01
+            isTrue = true;
+        elseif abs(v) < 0.001
+            isTrue = false;
+        else
+            isTrue = false;
+        end
         return;
+    end
+    
+    if line2(2, 1) == line2(1, 1) && line1(2, 1) ~= line1(1, 1)
+        b = line1(1,2)-m1*line1(1,1);
+        t = line2(1, 1)*m1 + b;
+        v = (line2(1, 2) - t) * (line2(2, 2) - t);
+        if v < 0 && abs(line2(1, 2) - t)> 0.01 && abs(line2(2, 2) - t) > 0.01
+            isTrue = true;
+        elseif abs(v) < 0.001
+            isTrue = false;
+        else
+            isTrue = false;
+        end
+        return;
+    end
+    
+    % check parallel case
+    if closeTo(1/m1, 1/m2) || closeTo(m1, m2)
+        b1 = intersect(line1,m1);
+        b2 = intersect(line2,m2);
+        if closeTo(b1, b2)
+            isTrue = true;
+            return;
+        end
     end
         
 
-    intersect = @(line,m) line(1,2) - m*line(1,1);
+    
     b1 = intersect(line1,m1);
     b2 = intersect(line2,m2);
     xintersect = (b2-b1)/(m1-m2);
     yintersect = m1*xintersect + b1;
     
-    isPointInside = @(xint,myline) ...
-    (xint >= myline(1,1) && xint <= myline(2,1)) || ...
-    (xint >= myline(2,1) && xint <= myline(1,1));
     
-    inside = isPointInside(xintersect,line1) && ...
-         isPointInside(xintersect,line2) && ...
-         isPointInside(yintersect,line1) && ...
-         isPointInside(yintersect,line2);
+    
+    inside = isPointXInside(xintersect,line1) && ...
+         isPointXInside(xintersect,line2) && ...
+         isPointYInside(yintersect,line1) && ...
+         isPointYInside(yintersect,line2);
+     
+
+    if ( closeTo(xintersect,line1(1, 1)) && closeTo(yintersect,line1(1, 2)) ) ||...
+       ( closeTo(xintersect,line1(2, 1)) && closeTo(yintersect,line1(2, 2)) ) ||...
+       ( closeTo(xintersect,line2(1, 1)) && closeTo(yintersect,line2(1, 2)) ) ||...
+       ( closeTo(xintersect,line2(2, 1)) && closeTo(yintersect,line2(2, 2)) )
+        inside = false;
+    end
+
      
     if inside == false
-        isTrue = false;
+        
+%     figure;
+%     plot([line1(1,1), line1(2,1)],[line1(1,2), line1(2,2)], 'r');
+%     hold on;
+%     plot([line2(1,1), line2(2,1)],[line2(1,2), line2(2,2)], 'r');
+    isTrue = false;
     end
+   
     
 end
